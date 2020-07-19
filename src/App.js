70286a617322dfaa60bf75ff471efb10dde72e48
Henri <henri.silvatj@hotmail.com>
2020-07-18 22:17:11 -0300

    Implementado CRUD de repositorios; Finalizando o desafio;

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
-import React from "react";
+import React, { useState, useEffect } from "react"
 
-import "./styles.css";
+import "./styles.css"
+import api from "./services/api"
 
 function App() {
+
+  const [ repositories, setRepositories ] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, [])
+
+  /**
+   * Função para adicionar um repositório
+   */
   async function handleAddRepository() {
-    // TODO
+    /**
+     * Faz a requisição para a API para inserir o repositório
+     */
+    api.post('repositories', { title: 'Teste', url: 'https://github.com', techs: []}).then(response => {
+      /**
+       * Se deu tudo certo, atualiza o state
+       */
+      if (response.status === 200) {
+        setRepositories([
+          ...repositories,
+          {
+            id: response.data.id,
+            url: response.data.url,
+            title: response.data.title,
+            techs: response.data.techs,
+          }
+        ])
+      }
+    })
   }
 
+  /**
+   * Função para remover um repositório 
+   * @param int id 
+   */
   async function handleRemoveRepository(id) {
-    // TODO
+    /**
+     * Busca a posição que o repositório está no state, para remover ele depois
+     */
+    const repositoryIndex = repositories.findIndex(e => e.id === id)
+
+    /**
+     * Faz a requisição para a API para remover o repositório
+     */
+    api.delete(`repositories/${id}`).then(response => {
+      /**
+       * Se deu tudo certo, remove do state
+       */
+      if (response.status === 204) {
+        const data = repositories
+        data.splice(repositoryIndex, 1)
+
+        setRepositories([ ...data ])
+      }
+    })
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+            <li key={repository.id}>
+            {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+          )   
+        )}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
@@ -28,4 +84,4 @@ function App() {
   );
 }
 
-export default App;
+export default App
